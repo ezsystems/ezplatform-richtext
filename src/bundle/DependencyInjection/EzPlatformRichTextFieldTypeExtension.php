7b50eb93d6611b4cffe287cb35010f857926f8d8
Andrew Longosz <andrzej.longosz@ez.no>
2018-02-07 13:46:06 +0100

    [Symfony] Created EzPlatformRichTextFieldTypeBundle

---
+++ src/bundle/DependencyInjection/EzPlatformRichTextFieldTypeExtension.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformRichTextFieldTypeBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Config\FileLocator;
+
+/**
+ * eZ Platform RichText Field Type Bundle extension.
+ */
+class EzPlatformRichTextFieldTypeExtension extends Extension
+{
+    /**
+     * Load eZ Platform RichText Field Type Bundle configuration.
+     *
+     * @param array $configs
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../Resources/config')
+        );
+        $loader->load('services.yml');
+    }
+}
