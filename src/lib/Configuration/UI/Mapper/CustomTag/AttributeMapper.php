dabf235c52e573e7a79eea442ca833d1d752a4eb
Andrew Longosz <alongosz@users.noreply.github.com>
2019-08-19 17:07:57 +0200

    Implemented UI configuration mappers
    Moved from AdminUI

---
+++ src/lib/Configuration/UI/Mapper/CustomTag/AttributeMapper.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformRichText\Configuration\UI\Mapper\CustomTag;
+
+/**
+ * Map RichText Custom Tag attribute of supported type to proper UI config.
+ *
+ * @internal For internal use by RichText package
+ */
+interface AttributeMapper
+{
+    /**
+     * Check if mapper supports given Custom Tag attribute type.
+     *
+     * @param string $attributeType
+     *
+     * @return bool
+     */
+    public function supports(string $attributeType): bool;
+
+    /**
+     * Map Configuration for the given Custom Tag attribute type.
+     *
+     * @param string $tagName
+     * @param string $attributeName
+     * @param array $customTagAttributeProperties
+     *
+     * @return array Mapped attribute configuration
+     */
+    public function mapConfig(
+        string $tagName,
+        string $attributeName,
+        array $customTagAttributeProperties
+    ): array;
+}
---
+++ src/lib/Configuration/UI/Mapper/CustomTag/CommonAttributeMapper.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformRichText\Configuration\UI\Mapper\CustomTag;
+
+/**
+ * Map RichText Custom Tag attribute of any type to proper UI config.
+ *
+ * @internal For internal use by RichText package
+ */
+class CommonAttributeMapper implements AttributeMapper
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function supports(string $attributeType): bool
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function mapConfig(
+        string $tagName,
+        string $attributeName,
+        array $customTagAttributeProperties
+    ): array {
+        return [
+            'label' => "ezrichtext.custom_tags.{$tagName}.attributes.{$attributeName}.label",
+            'type' => $customTagAttributeProperties['type'],
+            'required' => $customTagAttributeProperties['required'],
+            'defaultValue' => $customTagAttributeProperties['default_value'],
+        ];
+    }
+}
