15370005a3ce2dded0e2544569574a320b7f6845
Kamil Madejski <kamil.madejski@ez.no>
2019-08-23 10:23:56 +0200

    Use realpath instead of manual replacement

--- src/lib/eZ/RichText/Converter/Xslt.php
+++ src/lib/eZ/RichText/Converter/Xslt.php
@@ -78,8 +78,7 @@ class Xslt extends XmlBase implements Converter
 
             // Prevents showing XSLTProcessor::importStylesheet() warning on Windows file system
             // & allows loading stylesheets if project's directory name contains space(s)
-            $hrefAttr->value = str_replace(['\\', ' '], ['/', '%20'], $stylesheet);
-
+            $hrefAttr->value = realpath($stylesheet);
             $newEl->appendChild($hrefAttr);
             $xslDoc->documentElement->insertBefore($newEl, $insertBeforeEl);
         }
