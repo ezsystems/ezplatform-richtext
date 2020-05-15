a6492273fdabaa0818be23be2ec753addc1cd4cc
Serhey Dolgushev <dolgushev.serhey@gmail.com>
2020-05-15 12:30:25 +0100

    EZP-31632: Floating table toolbar

--- src/bundle/Resources/public/js/OnlineEditor/toolbars/config/base-table.js
+++ src/bundle/Resources/public/js/OnlineEditor/toolbars/config/base-table.js
@@ -1,18 +1,18 @@
-import AlloyEditor from 'alloyeditor';
-import EzConfigButtonsBase from './base-buttons';
+import EzConfigBase from './base';
 
-export default class EzConfigTableBase extends EzConfigButtonsBase {
+export default class EzConfigTableBase extends EzConfigBase {
     constructor(config) {
         super(config);
 
         this.name = this.getConfigName();
         this.buttons = this.getButtons(config);
-
-        this.getArrowBoxClasses = AlloyEditor.SelectionGetArrowBoxClasses.table;
-        this.setPosition = AlloyEditor.SelectionSetPosition.table;
     }
 
     getConfigName() {
+        return '';
+    }
+
+    getArrowBoxClasses() {
         return '';
     }
 }
