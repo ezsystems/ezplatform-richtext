09f99b8a386701d39ba5b599c7c0f8ce6f601688
Serhey Dolgushev <dolgushev.serhey@gmail.com>
2020-09-29 15:46:13 -0400

    Revert "Fix related to https://github.com/ezsystems/ezplatform-richtext/pull/163"
    This reverts commit b6760838ec6582c3190ba3fc5ca622343179949d.

--- src/bundle/Resources/public/js/OnlineEditor/buttons/ez-btn-linkedit.js
+++ src/bundle/Resources/public/js/OnlineEditor/buttons/ez-btn-linkedit.js
@@ -133,23 +133,19 @@ export default class EzBtnLinkEdit extends Component {
      */
     showUI() {
         const nativeEditor = this.props.editor.get('nativeEditor');
-        const eventOptions = {
+        const element = nativeEditor.elementPath().lastElement;
+
+        nativeEditor.fire('editorInteraction', {
             editor: this.props.editor,
+            nativeEvent: {
+                editor: this.props.editor,
+                target: element.$,
+            },
             selectionData: {
                 element: this.state.element,
                 region: this.getRegion(),
             },
-        };
-        const path = nativeEditor.elementPath();
-
-        if (path) {
-            eventOptions.nativeEvent = {
-                editor: this.props.editor,
-                target: path.lastElement.$,
-            };
-        }
-
-        nativeEditor.fire('editorInteraction', eventOptions);
+        });
     }
 
     /**
