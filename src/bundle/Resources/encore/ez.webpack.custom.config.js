0b23c0b30746879be62be05f0807ce15b2ff6f07
Dariusz Szut <dew326@gmail.com>
2021-02-17 12:17:38 +0100

    EZP-32347: As an Editor, I want to see how many characters are in ric… (#175)
    * EZP-32347: As an Editor, I want to see how many characters are in richtext

---
+++ src/bundle/Resources/encore/ez.webpack.custom.config.js
@@ -0,0 +1,52 @@
+const Encore = require('@symfony/webpack-encore');
+const path = require('path');
+const { styles } = require('@ckeditor/ckeditor5-dev-utils');
+
+Encore.reset();
+Encore.setOutputPath('public/assets/richtext/build')
+    .setPublicPath('/assets/richtext/build')
+    .enableSassLoader()
+    .disableSingleRuntimeChunk();
+
+Encore.addEntry('ezplatform-richtext-onlineeditor-js', [
+    path.resolve(__dirname, '../public/js/CKEditor/core/base-ckeditor.js'),
+]).addStyleEntry('ezplatform-richtext-onlineeditor-css', [path.resolve(__dirname, '../public/scss/alloyeditor.scss')]);
+
+const customConfig = Encore.getWebpackConfig();
+
+customConfig.name = 'richtext';
+
+customConfig.module.rules.push({
+    test: /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
+    use: ['raw-loader'],
+});
+
+customConfig.module.rules.push({
+    test: /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css$/,
+    use: [
+        {
+            loader: 'style-loader',
+            options: {
+                injectType: 'singletonStyleTag',
+                attributes: {
+                    'data-cke': true,
+                },
+            },
+        },
+        {
+            loader: 'postcss-loader',
+            options: styles.getPostCssConfig({
+                themeImporter: {
+                    themePath: require.resolve('@ckeditor/ckeditor5-theme-lark'),
+                },
+                minify: true,
+            }),
+        },
+    ],
+});
+
+customConfig.module.rules[1] = {};
+customConfig.module.rules[2] = {};
+
+// Config or array of configs: [customConfig1, customConfig2];
+module.exports = customConfig;
---
+++ src/bundle/Resources/public/js/CKEditor/plugins/character-counter.js
@@ -0,0 +1,70 @@
+import Plugin from '../../../../../../../../ezplatform-admin-ui-assets/Resources/public/vendors/@ckeditor/ckeditor5-core/src/plugin';
+
+const TEXT_NODE = 3;
+
+class CharacterCounter extends Plugin {
+    splitIntoWords(text) {
+        return text.split(' ').filter((word) => word.trim());
+    }
+
+    sanitize(text) {
+        return text.replace(/[\u200B-\u200D\uFEFF]/g, '');
+    }
+
+    iterateThroughChildNodes(node, callback) {
+        if (typeof node.getAttribute === 'function' && node.getAttribute('data-ezelement') === 'ezconfig') {
+            return;
+        }
+
+        callback(node);
+        node = node.firstChild;
+
+        while (node) {
+            this.iterateThroughChildNodes(node, callback);
+            node = node.nextSibling;
+        }
+    }
+
+    getTextNodeValues(node) {
+        let values = [];
+        const pushValue = (node) => {
+            if (node.nodeType === TEXT_NODE) {
+                const nodeValue = this.sanitize(node.nodeValue);
+
+                values = values.concat(this.splitIntoWords(nodeValue));
+            }
+        };
+
+        this.iterateThroughChildNodes(node, pushValue);
+
+        return values;
+    }
+
+    countWordsCharacters(container, editorHtml) {
+        const counterWrapper = container.parentElement.querySelector('.ez-character-counter');
+
+        if (counterWrapper) {
+            const wordWrapper = counterWrapper.querySelector('.ez-character-counter__word-count');
+            const charactersWrapper = counterWrapper.querySelector('.ez-character-counter__character-count');
+            const words = this.getTextNodeValues(editorHtml);
+
+            wordWrapper.innerText = words.length;
+            charactersWrapper.innerText = words.join(' ').length;
+        }
+    }
+
+    init() {
+        this.editor.model.document.on('change:data', () => {
+            const data = this.editor.getData();
+            const div = document.createElement('div');
+            const documentFragment = document.createDocumentFragment();
+
+            div.innerHTML = data;
+            documentFragment.appendChild(div);
+
+            this.countWordsCharacters(this.editor.sourceElement, documentFragment);
+        });
+    }
+}
+
+export default CharacterCounter;
