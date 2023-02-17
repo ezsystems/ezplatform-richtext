ca16b5ee7a73b7d6790fc4675b998606c419966f
Paweł Niedzielski <pawel.tadeusz.niedzielski@gmail.com>
2023-02-17 17:42:45 +0100

    Added missing PHPStan analysis

---
+++ ignore-by-php-version.neon.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+$includes = [];
+if (PHP_VERSION_ID < 80000) {
+    $includes[] = __DIR__ . '/ignore-lte-php7.4-errors.neon';
+} else {
+    $includes[] = __DIR__ . '/ignore-gte-php8.0-errors.neon';
+}
+
+$config = [];
+$config['includes'] = $includes;
+
+return $config;
